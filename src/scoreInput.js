0bf2777d761b781532ebb7522a63b53a31248dec
Cesar Nina <c3sar207@gmail.com>
2023-09-17 22:07:19 -0400

    created a foundation to start the logic

---
+++ src/scoreInput.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export const ScoreInput = () => {
+  const [us, setUs] = useState(0);
+
+  return (
+    <div className="input_wrapper">
+      <label>
+        Them
+        <input name="them" type="number" />
+      </label>
+
+      <label>
+        Us
+        <input name="Us" type="number" />
+      </label>
+
+      <button>Add Round</button>
+    </div>
+  );
+};
