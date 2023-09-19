52c4d64d51670a54228a3bfe1b2114e56a7d40db
Cesar Nina <c3sar207@gmail.com>
2023-09-18 20:00:05 -0400

    added a state for the "them" team

--- src/scoreInput.js
+++ src/scoreInput.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export const ScoreInput = () => {
   const [us, setUs] = useState(0);
+  const [them, setThem] = useState(0);
 
   return (
     <div className="input_wrapper">
