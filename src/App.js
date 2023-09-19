9d8c1eb29fe31ff1e0d2cd14d2f026063f9fa5dd
Cesar Nina <c3sar207@gmail.com>
2023-09-18 23:10:17 -0400

    added the state and component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { ScoreInput } from "./ScoreInput.js";
+import { ScoreList } from "./ScoreList";
 
-function App() {
-  return <ScoreInput />;
-}
+const App = () => {
+  const [Round, setRound] = useState(1);
+  const [us, setUs] = useState(0);
+  const [them, setThem] = useState(0);
+  const [score, setScore] = useState([]);
+
+  return (
+    <>
+      <ScoreInput />
+      <ScoreList />
+    </>
+  );
+};
 
 export default App;
