df29ed048c2e27536c7abd6bd3e7f5e18b3e19cc
Cesar Nina <c3sar207@gmail.com>
2023-09-24 17:23:24 -0400

    updated the data to return body of a table

--- src/components/ScoreList.js
+++ src/components/ScoreList.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-export const ScoreList = ({ Round, them, us }) => {
+export const ScoreList = ({ Round, score }) => {
   return (
-    <div className="list_wrapper">
-      <p>{Round}</p>
-      <p>{them}</p>
-      <p>{us}</p>
-      <button>Delete</button>
-    </div>
+    <tr>
+      <th>R{(Round = 1)}</th>
+      <td>{score.them}</td>
+      <td>{score.us}</td>
+    </tr>
   );
 };
