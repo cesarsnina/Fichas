263d6182d415d6f07656eb453f3239f5426ace76
Cesar Nina <c3sar207@gmail.com>
2023-09-29 20:24:49 -0400

    pass the proper props into the components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import { Header, ScoreInput, ScoreTable } from "./components";
 import "./App.css";
 
 const App = () => {
-  const [us, setUs] = useState(0);
   const [them, setThem] = useState(0);
+  const [us, setUs] = useState(0);
+  const [scores, setScores] = useState([[0, 0]]);
+
+  const submitScore = () => {
+    setScores([them, us]);
+    setThem(0);
+    setUs(0);
+  };
 
   return (
     <div className="app-wrapper">
       <Header />
-      <ScoreInput />
-      <ScoreTable score={{ them, us }} />
+      <ScoreInput
+        submitScore={submitScore}
+        score={{ them, us, setThem, setUs }}
+      />
+      <ScoreTable roundScores={scores} />
     </div>
   );
 };
