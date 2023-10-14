0ebe2a9ab08cfeea1e514008d1409d211d4f1873
Cesar Nina <c3sar207@gmail.com>
2023-10-13 21:23:04 -0400

    added a button to delete the row

--- src/components/ScoreList.js
+++ src/components/ScoreList.js
@@ -6,6 +6,7 @@ export const ScoreList = ({ roundScore, round }) => {
       <th>R{round + 1}</th>
       <td>{roundScore[0] || 0}</td>
       <td>{roundScore[1] || 0}</td>
+      <td>Delete</td>
     </tr>
   );
 };
